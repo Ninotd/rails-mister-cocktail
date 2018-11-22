12ad97946292cb7d38e387a9f3afd0a9cac7114d
Nino Raymond <nino.raymond@hec.edu>
2018-11-22 17:26:53 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'json'
+require 'open-uri'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+hash = ingredient["drinks"]
+hash.each do |element|
+  new_ingredient = Ingredient.new(name: element["strIngredient1"])
+  new_ingredient.save!
+end
+
+
+
+
